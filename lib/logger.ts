d5c6d7213697d835c7421aff4797e936c998a554
Saikot hasan <49225310+saikothasan@users.noreply.github.com>
2024-12-29 12:22:57 +0600

    Update logger.ts

--- lib/logger.ts
+++ lib/logger.ts
@@ -3,7 +3,9 @@ import pino from 'pino'
 export const logger = pino({
   level: process.env.LOG_LEVEL || 'info',
   transport: {
-    target: 'pino-pretty',
+    target: 'pino-pretty',  // Set target to 'pino-pretty'
+    options: {
+      colorize: true, // Optional: Colorize the output (for better readability)
+    },
   },
 })
-
